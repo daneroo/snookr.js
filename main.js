ebd41e509c955202cc6fa82df81763f2eede2b8c
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-11-13 20:14:00 -0500

    update to exif-0.4

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
 var ExifImage = require('exif').ExifImage;
 
 try {
-  new ExifImage({ image : 'images/2012-09-12T18.52.43.jpg' }, function (error, image) {
+  new ExifImage({ image : 'images/2012-09-12T18.52.43.jpg' }, function (error, exifData) {
     if (error) {
       console.log('Error: '+error.message);
-    } else{
-      pretty(image);
+      process.exit(-1);
     }
+    console.log('exif',exifData)
   });
 } catch (error) {
   console.log('Error: ' + error);
 }
 
-function pretty(image){
-  // console.log(image); // Do something with your data!
-  Object.keys(image).forEach(function(key){
-    console.log('exif section:',key);
-    var section = image[key];
-    if (section) section.forEach(function(tag){
-      // console.log('--',tag);
-      console.log('--',tag.tagName,':',tag.value);
-    });
-  });
-}
